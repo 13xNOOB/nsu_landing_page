d190fdb657d4c59ff741ee8d6058d1e7794daf56
AnonTheLastAirbender <163524714+AnonTheLastAirbender@users.noreply.github.com>
2025-05-31 14:51:58 +0600

    Overlay menu working separately, need to implement as chunk

--- combine.js
+++ combine.js
@@ -5,6 +5,7 @@ const chunksDir = path.join(__dirname, 'chunks');
 const outputFile = path.join(__dirname, 'index.html');
 
 const chunkFiles = [
+  'header_scripts.html',
   'utility_bar_asad.html',
   'header_asad.html',
   'slider_asad.html',
@@ -25,13 +26,14 @@ const template = `
   <title>NSU Landing Page</title>
   <link href="./output.css" rel="stylesheet">
   <script defer src="node_modules/alpinejs/dist/cdn.min.js"></script>
+  ${fs.readFileSync(path.join(chunksDir, 'header_scripts.html'), 'utf8')}
 </head>
-<body x-data>
+<body class="min-h-screen bg-gray-100" x-data="{ menuOpen: false }">
   <div id="app">
-    ${chunkFiles.map(file => {
-      const content = fs.readFileSync(path.join(chunksDir, file), 'utf8');
-      return content;
-    }).join('\n')}
+  ${chunkFiles
+    .filter(file => file !== 'header_scripts.html')
+    .map(file => fs.readFileSync(path.join(chunksDir, file), 'utf8'))
+    .join('\n')}
   </div>
 </body>
 </html>
