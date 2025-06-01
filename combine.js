bb4c4005809ae55f076dce3b800087c2cfffa03d
Hafiz <hafiz.asad@northsouth.edu>
2025-06-01 22:04:54 +0600

    Almost all of the parts working now. Need to fix: 1. WHAT'S HAPPENING AT NSU 2. Footer

--- combine.js
+++ combine.js
@@ -12,6 +12,7 @@ const chunkFiles = [
   'quick_links_asad.html',
   'achievements_and_accreditations_asad.html',
   'news_asad.html',
+  'news_stories.html',
   'upcoming_events_asad.html',
   'degree_asad.html',
   'footer_asad.html',
@@ -25,6 +26,9 @@ const template = `
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>NSU Landing Page</title>
+  <link href="https://fonts.googleapis.com/css2?family=Barlow+Semi+Condensed:wght@400;500;600&display=swap" rel="stylesheet">
+  <!-- FontAwesome (replace with your kit) -->
+  <script src="https://kit.fontawesome.com/025119980b.js" crossorigin="anonymous"></script>
   <link href="./output.css" rel="stylesheet">
   <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
   <script src="https://cdn.tailwindcss.com"></script>
