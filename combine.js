73a6556349ffe3eb4327b647ea6f8e6dec61ad45
AnonTheLastAirbender <163524714+AnonTheLastAirbender@users.noreply.github.com>
2025-06-01 06:10:16 +0600

    First part of the landing page working. Menus: 1. Utility Bar 2. Header and Overlay 3. Asset Slider for first part

--- combine.js
+++ combine.js
@@ -5,7 +5,7 @@ const chunksDir = path.join(__dirname, 'chunks');
 const outputFile = path.join(__dirname, 'index.html');
 
 const chunkFiles = [
-  'header_scripts.html',
+  'header_styles.html',
   'utility_bar_asad.html',
   'header_asad.html',
   'slider_asad.html',
@@ -14,7 +14,8 @@ const chunkFiles = [
   'news_asad.html',
   'upcoming_events_asad.html',
   'degree_asad.html',
-  'footer_asad.html'
+  'footer_asad.html',
+  'bottom_scripts.html'
 ];
 
 const template = `
@@ -25,15 +26,16 @@ const template = `
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>NSU Landing Page</title>
   <link href="./output.css" rel="stylesheet">
-  <script defer src="node_modules/alpinejs/dist/cdn.min.js"></script>
-  ${fs.readFileSync(path.join(chunksDir, 'header_scripts.html'), 'utf8')}
+  <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+  <script src="https://cdn.tailwindcss.com"></script>
+  ${fs.readFileSync(path.join(chunksDir, 'header_styles.html'), 'utf8')}
 </head>
-<body class="min-h-screen bg-gray-100" x-data="{ menuOpen: false }">
+<body class="min-h-screen bg-gray-100 flex flex-col" x-data="{ menuOpen: false }">
   <div id="app">
-  ${chunkFiles
-    .filter(file => file !== 'header_scripts.html')
-    .map(file => fs.readFileSync(path.join(chunksDir, file), 'utf8'))
-    .join('\n')}
+    ${chunkFiles
+      .filter(file => file !== 'header_styles.html')
+      .map(file => fs.readFileSync(path.join(chunksDir, file), 'utf8'))
+      .join('\n')}
   </div>
 </body>
 </html>
